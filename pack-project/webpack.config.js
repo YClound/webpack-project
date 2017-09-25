824f719acce5e88e483705b1c551f0a6f7814f3d
yanan.g055 <yanan.g055@shulidata.com>
2017-09-25 17:39:05 +0800

    html加载改为ejs

--- pack-project/webpack.config.js
+++ pack-project/webpack.config.js
@@ -1,23 +1,61 @@
+var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // --config,chunkhash,publicPath
 module.exports = {
-	entry: {
-		main: './src/script/main.js',
-		a: './src/script/a.js'
-	},
-	output: {
-		path:  __dirname +'/dist',
-		filename: 'src/script/[name].[chunkhash].js'
-	},
-	plugins: [
-		new htmlWebpackPlugin({
-			template: 'index.html',
-			title: 'webpack is good',
-			minify: {
-				removeComments: true,//删除注释
-				collapseWhitespace: true,//删除空格
-			}
-		})
-	]
+    entry: {
+        main: './src/script/main.js',
+        a: './src/script/a.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'src/script/[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new htmlWebpackPlugin({
+            template: 'index.ejs',
+            title: 'webpack is good',
+            minify: {
+                removeComments: true,//删除注释
+                collapseWhitespace: true,//删除空格
+            }
+        })
+    ]
 }
