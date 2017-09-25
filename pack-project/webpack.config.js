e6b92be6abf2344b30472543d7ae83f5dde3d315
iapplesex <iapplesex@163.com>
2017-09-25 23:38:33 +0800

    webpack处理es6

--- pack-project/webpack.config.js
+++ pack-project/webpack.config.js
@@ -2,18 +2,31 @@ var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-// --config,chunkhash,publicPath
+// --config,chunkhash,publicPath,chunks指定多个页面的js文件，excludeChuncks
 module.exports = {
     entry: {
         main: './src/script/main.js',
-        a: './src/script/a.js'
+        a: './src/script/a.js',
+        app: './src/app.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'src/script/[name].[chunkhash].js'
     },
     module: {
-        rules: [
+        loader: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel'
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
             {
                 test: /\.css$/,
                 use: [
@@ -51,6 +64,8 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         new htmlWebpackPlugin({
             template: 'index.ejs',
+            filename: 'index.html',
+            inject: 'body',
             title: 'webpack is good',
             minify: {
                 removeComments: true,//删除注释
