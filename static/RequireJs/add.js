d82b6fc88e76a6c586a939dbc034f5877b70f2f8
iappapp <iadore@live.cn>
2020-09-24 23:09:25 +0800

    ppt update

--- static/RequireJs/add.js
+++ static/RequireJs/add.js
@@ -1,9 +1,17 @@
 
 console.log('add 加载');
 
+/**
+ * define(id?, dependencies, factory)
+ * id: 定义中模块的名字 默认文件名
+ * dependencies：模块所依赖模块的数组
+ * factory：模块初始化要执行的函数或对象
+ */
+
 define([
     'data',
-], function (dataModule) {
+    'exports'
+], function (dataModule, exports) {
     console.log('add 执行', dataModule,  +new Date());
 
     // const add = (a = dataModule.a, b = dataModule.b) => { return a + b; }
@@ -11,7 +19,9 @@ define([
     // 未使用的模块 引用也会被加载
     const add = (a, b) => { return a + b; }
 
-    return {
-        add
-    }
+    exports.add = add
+
+    // return {
+    //     add
+    // }
 });
